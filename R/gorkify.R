3a00f79a27ff0a92cc217587296d9755470cf486
Diogo Marques <diogohomemmarques@gmail.com>
2019-07-12 08:53:27 +0100

    rename judeo christian helpers

--- R/gorkify.R
+++ R/gorkify.R
@@ -1,19 +1,19 @@
 # Aspect ratio
-aspect_ratio = function() {
+.aspect_ratio = function() {
   1 / 3
 }
 
 # Helper to fix aspect ration to 3:1. Returns old plot area values, for
 # resetting.
-fix_plot_area = function() {
+.fix_plot_area = function() {
   previous_pin = graphics::par("pin")
   graphics::par(pin = c(previous_pin[1],
-                        previous_pin[1] * aspect_ratio()))
+                        previous_pin[1] * .aspect_ratio()))
   previous_pin
 }
 
 # Helper to remove left and righ margins.
-remove_plot_margins = function() {
+.remove_plot_margins = function() {
   previous_mar = graphics::par("mar")
   graphics::par("mar" = c(previous_mar[1], 0,
                           previous_mar[3], 0))
@@ -21,14 +21,14 @@ remove_plot_margins = function() {
 }
 
 # Helper to reset par options after plotting
-reset_plot_dimensions = function(previous_plot_region,
+.reset_plot_dimensions = function(previous_plot_region,
                                  previous_margins) {
   graphics::par(pin = previous_plot_region)
   graphics::par(mar = previous_margins)
 }
 
 # Helper to start 1x1 empty plot
-start_plot = function() {
+.start_plot = function() {
   graphics::plot(0, xlim = 0:1, ylim = 0:1,
                  type = "n", bty = "n",
                  xaxt = "n", yaxt = "n",
@@ -36,7 +36,7 @@ start_plot = function() {
 }
 
 # Helper to add axis arrow
-add_axis = function(axis_lwd, axis_arrow_size, axis_labels_h_offset) {
+.add_axis = function(axis_lwd, axis_arrow_size, axis_labels_h_offset) {
   xstart = 0
   xend = 1 - axis_labels_h_offset - axis_arrow_size
   y = 0.5
@@ -44,13 +44,13 @@ add_axis = function(axis_lwd, axis_arrow_size, axis_labels_h_offset) {
                      lwd = axis_lwd, lend = 2)
   axis_arrow_size
   graphics::polygon(x = c(xend, xend, xend + axis_arrow_size),
-                    y = c(y + axis_arrow_size / 2 / aspect_ratio(),
-                          y - axis_arrow_size / 2 / aspect_ratio(), y),
+                    y = c(y + axis_arrow_size / 2 / .aspect_ratio(),
+                          y - axis_arrow_size / 2 / .aspect_ratio(), y),
                     col = 1)
 }
 
 # Helper to add axis labels
-add_axis_labels <- function(from, to, family, col_from, col_to,
+.add_axis_labels <- function(from, to, family, col_from, col_to,
                             axis_labels_cex, axis_labels_font,
                             axis_labels_h_offset, axis_labels_v_offset) {
   y = 0.5 + axis_labels_v_offset
@@ -65,7 +65,7 @@ add_axis_labels <- function(from, to, family, col_from, col_to,
 }
 
 # Helper to add "what" label
-add_what_label <- function(where, what, family, what_label_font, col_what,
+.add_what_label <- function(where, what, family, what_label_font, col_what,
                            what_label_v_offset, what_label_cex) {
   where_y = 0.5 - what_label_v_offset
   graphics::text(x = where, y = where_y, labels = what,
@@ -74,7 +74,7 @@ add_what_label <- function(where, what, family, what_label_font, col_what,
 }
 
 # Helper to add ellipse around label
-add_what_ellipse <- function(where, what, what_label_v_offset,
+.add_what_ellipse <- function(where, what, what_label_v_offset,
                              what_label_cex, what_ellipse_lwd) {
   major_axis = graphics::strwidth(what, "user", cex = what_label_cex)
   minor_axis = graphics::strheight(what, "user", cex = what_label_cex) * 2.75
@@ -87,7 +87,7 @@ add_what_ellipse <- function(where, what, what_label_v_offset,
 
 # Helper to add arrows coming out of ellipse and respective
 # question marks.
-add_what_arrows <- function(where, what_label_v_offset, what, what_label_cex,
+.add_what_arrows <- function(where, what_label_v_offset, what, what_label_cex,
                             what_arrow_length, what_arrow_gap_lh,
                             what_arrow_gap_rh, what_question_mark_gap,
                             what_arrow_head_size, family, what_question_cex) {
@@ -102,7 +102,7 @@ add_what_arrows <- function(where, what_label_v_offset, what, what_label_cex,
   arrow_end_right = segment_end_right - what_arrow_head_size
   # ys
   y = 0.5 - what_label_v_offset
-  what_arrow_y_size = what_arrow_head_size / aspect_ratio()
+  what_arrow_y_size = what_arrow_head_size / .aspect_ratio()
   # add segments
   graphics::segments(
     x0 = c(segment_start_left, segment_start_right),
@@ -196,21 +196,21 @@ gorkagram = function(
   what_ellipse_lwd = 1.5,
   what_question_mark_gap = 0.05,
   what_question_cex = 1.25) {
-  old_plot_margins = remove_plot_margins()
-  old_plot_region = fix_plot_area()
-  start_plot()
-  add_axis(axis_lwd, axis_arrow_size, axis_labels_h_offset)
-  add_axis_labels(from, to, family, col_from, col_to, axis_labels_cex,
+  old_plot_margins = .remove_plot_margins()
+  old_plot_region = .fix_plot_area()
+  .start_plot()
+  .add_axis(axis_lwd, axis_arrow_size, axis_labels_h_offset)
+  .add_axis_labels(from, to, family, col_from, col_to, axis_labels_cex,
                   axis_labels_font, axis_labels_h_offset,
                   axis_labels_v_offset)
-  add_what_label(where, what, family, what_label_font, col_what,
+  .add_what_label(where, what, family, what_label_font, col_what,
                  what_label_v_offset, what_label_cex)
-  add_what_ellipse(where, what, what_label_v_offset, what_label_cex,
+  .add_what_ellipse(where, what, what_label_v_offset, what_label_cex,
                    what_ellipse_lwd)
-  add_what_arrows(where, what_label_v_offset, what, what_label_cex,
+  .add_what_arrows(where, what_label_v_offset, what, what_label_cex,
                   what_arrow_length, what_arrow_gap_lh, what_arrow_gap_rh,
                   what_question_mark_gap, what_arrow_head_size, family,
                   what_question_cex)
-  reset_plot_dimensions(old_plot_region, old_plot_margins)
+  .reset_plot_dimensions(old_plot_region, old_plot_margins)
   invisible()
 }
